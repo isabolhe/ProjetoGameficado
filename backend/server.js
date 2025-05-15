44d57d716078993288f57ea098bd906746b3065f
isabolhe <isabelledejesus12@gmail.com>
2025-05-15 17:06:26 -0300

    :truck: mudei a ordem das pastas para rodar o react

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');  // Added for JWT
+const path = require('path');
 const app = express();
 const port = 3000;
+
 
 app.use(cors());
 app.use(express.json());
